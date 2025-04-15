440e90aebb90d033847e18471a2367e9ac974b33
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-04-16 01:49:37 +0500

    add promises

--- AdvancedTopics.js
+++ AdvancedTopics.js
@@ -27,20 +27,44 @@
 // calculator(10, 0, division);   // Result: ❌ Cannot divide by zero
 
 
-//callback hell
-function getData(dataId, nextData){
-    setTimeout(()=>{
-        console.log("data", dataId);
-        nextData();
-    },2000)
-}
-getData(1, ()=>{
-    console.log("fetching data 2") 
-    getData(2, ()=>{
-        console.log("fetching data 3")
-        getData(3, ()=>{
-            console.log("fetching data 4")  
-        })  
-    })  
-})
+// //callback hell
+// function getData(dataId, nextData){
+//     setTimeout(()=>{
+//         console.log("data", dataId);
+//         nextData();
+//     },2000)
+// }
+// getData(1, ()=>{
+//     console.log("fetching data 2") 
+//     getData(2, ()=>{
+//         console.log("fetching data 3")
+//         getData(3, ()=>{
+//             console.log("fetching data 4")  
+//         })  
+//     })  
+// })
 
+// to avoid callback hell we use Promises
+//promise three states: fullfilled or resolved, reject, pending
+//promise we don't create but they are used or consumed when we fetch data from apis then promises are made
+// let myPromise = new Promise((resolve,reject)=>{ //resolve and reject are callbacks
+// console.log("I am a promise")
+// resolve(4)
+// reject("error")
+// })
+// console.log(myPromise)
+
+const getPromise = () => {
+    return new Promise((resolve,reject)=>{
+        console.log("I am a promise");
+        resolve("success"); //if commented this line, promise will return failure
+        reject("failure");
+    })      
+}
+let promise = getPromise();
+promise.then((res)=>{ 
+    console.log("promise fulfilled", res);
+})
+promise.catch((err)=>{
+    console.log("promise rejected", err);
+})
