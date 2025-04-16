7531104c8f29e2942326c61a690e9aa1396d2184
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-04-16 16:05:59 +0500

    add using async await

--- fetchApi.js
+++ fetchApi.js
@@ -1,4 +1,13 @@
 //data fetching through api and printing promise that is fetched from api
+// let url = "https://catfact.ninja/facts";
+// let promise = fetch(url);
+// console.log(promise);
+
+//using async await
 let url = "https://catfact.ninja/facts";
-let promise = fetch(url);
-console.log(promise);
+const getFacts = async ()=> {
+    console.log("Fetching data ....");
+    let response = await fetch(url)
+    console.log(response)
+}
+getFacts();
